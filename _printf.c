fecd3ac4f5164311303238a319bd062cc088478f
johnlivingprooff <johnlivingprooff@gmail.com>
2023-08-21 21:03:38 +0200

    Trying to merge with master

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _printf - Function that produces output according to a format.
+ * @format: the string to output
+ * Return: number of chars printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	print_it print_func[] = {
+		{"%c", print_char},
+		{"%s", print_str},
+		{"%%", print_per},
+		{"%d", print_int},
+	};
+
+	int i = 0, r_len = 0;
+	/* stores the size of the print_func */
+	size_t j, s_num = sizeof(print_func) / sizeof(print_func[0]);
+
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		for (j = 0; j < s_num; j++)
+		{
+			if (format[i] == print_func[j].specifier[0]
+				&& format[i + 1] == print_func[j].specifier[1])
+			{
+				r_len += print_func[j].print(args);
+				i += 2;
+				break;
+			}
+		}
+		if (j < s_num)
+			continue;
+		putchar(format[i]);
+		r_len++;
+		i++;
+	}
+	va_end(args);
+
+	if (format[i] == '%' && format[i + 1] == '\0')
+		return (-1);
+
+	return (r_len);
+}
--- main.h
+++ main.h
@@ -1,8 +1,26 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* standard header files here */
+/* header files */
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
 
-/* function prototypes here */
+/* strutures for all cases */
+typedef struct printer
+{
+	const char *specifier;
+	int (*print)(va_list args);
+} print_it;
+
+/* prototypes */
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_per(va_list args);
+int print_int(va_list args);
 
 #endif /* MAIN_H */

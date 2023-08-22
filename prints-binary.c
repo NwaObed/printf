b137b09693d23694a28647d5245a8ee397df0594
johnlivingprooff <johnlivingprooff@gmail.com>
2023-08-23 00:35:49 +0200

    Added print-binary function

---
+++ prints-binary.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_binary - converts unsigned int to binary
+ * @args: list of args
+ * Return: number of output count
+ */
+int print_binary(va_list args)
+{
+	int b = va_arg(args, int);
+	int binary[32], bit;
+	int ind, i, count = 0;
+
+	if (b == 1)
+	{
+		_putchar(b + '0');
+		count++;
+	}
+	if (b == 0)
+	{
+		_putchar(b + '0');
+		count++;
+	}
+	while (b > 1)
+	{
+		bit = b % 2;
+		binary[ind++] = bit;
+		b /= 2;
+	}
+	for (i = ind - 1; i >= 0; i--)
+	{
+		_putchar(binary[i] + '0');
+		count++;
+	}
+	return (count);
+}

acf8dd8f1cdde4ade14b2d3d8b79cddc1cccee02
NwaObed <nwachukwobed@gmail.com>
2023-08-23 21:16:31 +0000

    update

--- handle_specifier.c
+++ handle_specifier.c
@@ -58,7 +58,7 @@ int print_int(va_list args __attribute__((unused)))
 	int i = va_arg(args, int);
 	int ind = 0, r;
 	int count = 0;
-	char result[2];
+	char result[10];
 	int dgt, j = i;
 	char *ptr;
 	
@@ -66,21 +66,23 @@ int print_int(va_list args __attribute__((unused)))
 		putchar('0' + i);
 	else
 	{
+/*		while (i >= 10)*/
+	/*	{*/
+		/*	i /= 10;*/
+/*			count++;*/
+/*		}*/
+/*		i = j;*/
 		while (i >= 10)
 		{
-			i /= 10;
-			count++;
-		}
-		i = j;
-		while (i >= 10)
-		{
+			printf("I am looping \n");
+			printf("Step: %d\n\n", i);
 			dgt = i % 10;
 			result[ind] = dgt;
 			i /= 10;
 			ind++;
 		}
 		result[ind++] = i;
-		printf("%c", result[0]);
+		printf("%c", 'o');
 		for (r = (strlen(result) - 1); r == 0; r--)
 		{
 			if (result[r] == '\0')

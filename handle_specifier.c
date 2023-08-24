6ec85795383310d56819fdc2ba7d14258bed987f
NwaObed <nwachukwobed@gmail.com>
2023-08-24 18:43:29 +0000

    cleaning

---
+++ handle_specifier.c
@@ -0,0 +1,135 @@
+#include "main.h"
+
+/**
+ * print_char - helper function to print char
+ * @args: list of arguments
+ * Return: 1 - length of char
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * print_str - helper function to print string
+ * @args: list of arguments
+ * Return: returns length of string
+ */
+int print_str(va_list args)
+{
+	char *s = va_arg(args, char *);
+	int i, s_len;
+
+	if (s == NULL)
+	{
+		s = "(null)";
+		for (i = 0; i < 6; i++)
+			_putchar(s[i]);
+		return (6);
+	}
+	s_len = strlen(s);
+	for (i = 0; i < s_len; i++)
+		_putchar(s[i]);
+	return (s_len);
+}
+
+/**
+ * print_per - helper function to print %
+ * @args: list of arguments
+ * Return: 1
+ */
+int print_per(va_list args __attribute__((unused)))
+{
+	char per = '%';
+
+	_putchar(per);
+	return (1);
+}
+
+/**
+ * print_int - prints the format string
+ * if no specifier is encountered
+ * @args: list of arguments
+ * Return: integer printed ?
+ */
+int print_int(va_list args __attribute__((unused)))
+{
+	int i = va_arg(args, int);
+	int ind = 0, r;
+	int count = 0;
+	char result[10];
+	int dgt, j = i;
+	char *ptr;
+
+	if (i < 10)
+		putchar('0' + i);
+	else
+	{
+		while (i >= 10)
+		{
+			dgt = i % 10;
+			result[ind] = dgt;
+			i /= 10;
+			ind++;
+		}
+		result[ind++] = i;
+		for (r = (strlen(result) - 1); r == 0; r--)
+		{
+			if (result[r] == '\0')
+				continue;
+			else
+			{
+				putchar(result[r]);
+			}
+		}
+	}
+	return (ind);
+}
+
+/**
+ * print_decimal - prints the format string
+ * if no specifier is encountered
+ * @args: list of arguments
+ * Return: integer printed ?
+ */
+int print_decimal(va_list args __attribute__((unused)))
+{
+	int d = va_arg(args, int);
+	int val = 0, dgt, tmp, count = 0;
+	int j, div = 1; 
+
+	if (d < 0)
+	{
+		_putchar('-');
+		d = -d;
+		count++;
+	}
+	if (d == INT_MIN)
+	{
+		_putchar('-');
+		d = INT_MAX;
+		count++;
+	}
+	tmp = d;
+	while (tmp > 0)
+	{
+		tmp /= 10;
+		val++;
+	}
+	for (j = 0; j < val - 1; j++)
+		div = div * 10;
+	while (div > 0)
+	{
+		dgt = d / div;
+		_putchar('0' + dgt);
+		d %= div;
+		div /= 10;
+		count++;
+	}
+	if (d == INT_MIN)
+		_putchar((d + 1) + '0');
+	return (count);
+}
---
+++ main.h
@@ -0,0 +1,34 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* header files */
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+
+/* strutures for all cases */
+typedef struct printer
+{
+	const char *specifier;
+	int (*print)(va_list args);
+} print_it;
+
+/* prototypes */
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_per(va_list args);
+int print_int(va_list args);
+int print_decimal(va_list args);
+int print_binary(va_list args);
+int print_unsigned_int(va_list args);
+int _loop_print(unsigned int num, int arrSize);
+unsigned int _convert_int_to_oct(unsigned int num);
+int print_unsigned_octal(va_list args);
+int get_byte(int num);
+
+#endif /* MAIN_H */

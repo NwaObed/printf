19e5b7225fd8fd1a78b478091cbf2923f3d4fadb
johnlivingprooff <johnlivingprooff@gmail.com>
2023-08-25 21:27:22 +0200

    Updating octal function

--- prints-octal.c
+++ prints-octal.c
@@ -9,16 +9,23 @@ int print_octal(va_list args)
 {
 	size_t o = va_arg(args, unsigned int);
 	char octal[32];
-	int ind = 0, i;
+	int ind = 0, i, count = 0;
 
+	if (o == 0)
+	{
+		_putchar('0');
+		count++;
+	}
 	while (o > 0)
 	{
 		octal[ind++] = (o % 8) + '0';
 		o /= 8;
+		count++;
 	}
 	for (i = ind - 1; i >= 0; i--)
 	{
 		_putchar(octal[i]);
+		count++;
 	}
-	return (ind);
+	return (count);
 }

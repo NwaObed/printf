c181361445af13183485ae0de3acc45f5096ccc2
NwaObed <nwachukwobed@gmail.com>
2023-08-25 17:35:42 +0000

    int length

--- main.h
+++ main.h
@@ -34,5 +34,6 @@ int print_unsigned_int(va_list args);
 int print_octal(va_list args);
 int print_hex(va_list args);
 int print_heX(va_list args);
+int _intlen_recursion(int num);
 
 #endif /* MAIN_H */

5fa075375846b840dd189fc94c72bb12bd8b9241
johnlivingprooff <johnlivingprooff@gmail.com>
2023-08-25 20:03:23 +0200

    Updating binary function

--- prints-binary.c
+++ prints-binary.c
@@ -9,23 +9,40 @@ int print_binary(va_list args)
 {
 	int b = va_arg(args, unsigned int);
 	int binary[32], bit;
+	int neg = 0;
 	int ind = 0, i, count = 0;
 
+	if (b < 0)
+	{
+		neg = 1;
+		b = -b;
+	}
 	if (b == 0)
 	{
 		_putchar('0');
 		count++;
 	}
-	while (b > 0)
+	else
 	{
-		bit = b % 2;
-		binary[ind++] = bit;
-		b /= 2;
+		while (b > 0)
+		{
+			bit = b % 2;
+			binary[ind++] = bit;
+			b /= 2;
+		}
+		if (neg)
+		{
+			while (ind < 32)
+			{
+				binary[ind++] = 1;
+			}
+		}
+
+		for (i = ind - 1; i >= 0; i--)
+		{
+			_putchar(binary[i] + '0');
+			count++;
+		}
 	}
-	for (i = ind - 1; i >= 0; i--)
-	{
-		_putchar(binary[i] + '0');
-		count++;
-	}
-	return (count);
+	return (count - 1);
 }
